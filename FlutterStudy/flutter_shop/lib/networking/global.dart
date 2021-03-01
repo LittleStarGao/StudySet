8227d1c3030d87fa94ebf40b00b066b350485d20
Jiajia.Gao <Jiajia.Gao@geely.com>
2021-03-01 16:40:54 +0800

    提交所有代码

---
+++ FlutterStudy/flutter_shop/lib/networking/global.dart
@@ -0,0 +1,5 @@
+class Global {
+  static const int CONNECT_TIMEOUT = 30000;
+  static const int RECEIVE_TIMEOUT = 30000;
+  static const String accessToken = 'data';
+}
---
+++ TwoFriendsFlutter/two_user_frineds/android/app/src/main/kotlin/com/example/two_user_frineds/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.two_user_frineds
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
